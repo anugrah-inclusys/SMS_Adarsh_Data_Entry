a9ee3bc5dc0a9a5b9d3f2d7a988369cbfca59d77
PRESISPREMACHANDRAN <presipremachandran@gmail.com>
2025-04-28 17:50:03 +0530

    special education assessment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { runEnquiryUpload } = require("./services/uploadEnquiry");
 const { runStaffUpload } = require("./services/uploadStaff");
 const { runAdmissionUpload } = require("./services/uploadAdmission");
+const { runSpecialEducationAssessmentUpload } = require("./services/uploadSpecialEducationAssessment");
 const { injectStudentIds } = require("./services/injectStudentIdIntoAdmission");
 const {
   runInitialAssessmentUpload,
@@ -9,19 +10,26 @@ const {
 (async () => {
   try {
     // await runStaffUpload();
-    await runEnquiryUpload();
+    // await runEnquiryUpload();
+    // await injectStudentIds({
+    //   admissionPath: "./data/enquiry.csv",
+    //   enquiryIdsPath: "./output/student_ids.xlsx",
+    //   outputPath: "./output/admission_with_ids.csv",
+    // });
+    // await runAdmissionUpload();
+    // await injectStudentIds({
+    //   admissionPath: "./data/initial_assessment.csv",
+    //   enquiryIdsPath: "./output/student_ids.xlsx",
+    //   outputPath: "./output/initial_assessment_with_ids.csv",
+    // });
+    // await runInitialAssessmentUpload();
     await injectStudentIds({
-      admissionPath: "./data/enquiry.csv",
+      admissionPath: "./data/special_education_assessment.csv",
       enquiryIdsPath: "./output/student_ids.xlsx",
-      outputPath: "./output/admission_with_ids.csv",
+      outputPath: "./output/special_education_assessment_with_ids.csv",
     });
-    await runAdmissionUpload();
-    await injectStudentIds({
-      admissionPath: "./data/initial_assessment.csv",
-      enquiryIdsPath: "./output/student_ids.xlsx",
-      outputPath: "./output/initial_assessment_with_ids.csv",
-    });
-    await runInitialAssessmentUpload();
+    await runSpecialEducationAssessmentUpload()
+
 
     console.log("🎉 Upload complete!");
   } catch (err) {
