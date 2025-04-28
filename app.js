da4036aeb433941d4ee530124f8b11ca1f37590b
anugrah-inclusys <anugrah.inclusys@gmail.com>
2025-04-28 18:22:03 +0530

    special-education-term first step

--- app.js
+++ app.js
@@ -5,23 +5,30 @@ const { injectStudentIds } = require("./services/injectStudentIdIntoAdmission");
 const {
   runInitialAssessmentUpload,
 } = require("./services/uploadInitialAssessment");
+const { runSpecialEducationTermUpload } = require("./services/uploadSpecialEducationTerm");
 
 (async () => {
   try {
     // await runStaffUpload();
-    await runEnquiryUpload();
+    // await runEnquiryUpload();
+    // await injectStudentIds({
+    //   admissionPath: "./data/enquiry.csv",
+    //   enquiryIdsPath: "./output/student_ids.xlsx",
+    //   outputPath: "./output/admission_with_ids.csv",
+    // });
+    // await runAdmissionUpload();
+    // await injectStudentIds({
+    //   admissionPath: "./data/initial_assessment.csv",
+    //   enquiryIdsPath: "./output/student_ids.xlsx",
+    //   outputPath: "./output/initial_assessment_with_ids.csv",
+    // });
+    // await runInitialAssessmentUpload();
     await injectStudentIds({
-      admissionPath: "./data/enquiry.csv",
+      admissionPath: "./data/special_education_term.csv",
       enquiryIdsPath: "./output/student_ids.xlsx",
-      outputPath: "./output/admission_with_ids.csv",
+      outputPath: "./output/special_education_term_with_ids.csv",
     });
-    await runAdmissionUpload();
-    await injectStudentIds({
-      admissionPath: "./data/initial_assessment.csv",
-      enquiryIdsPath: "./output/student_ids.xlsx",
-      outputPath: "./output/initial_assessment_with_ids.csv",
-    });
-    await runInitialAssessmentUpload();
+    await runSpecialEducationTermUpload()
 
     console.log("🎉 Upload complete!");
   } catch (err) {
